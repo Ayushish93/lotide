bfb26696474321e57d8c96db41e47720b4aae9bf
Ayushish93 <ayushi08sharma@gmail.com>
2020-09-15 02:51:32 +0000

    changes

---
+++ lotide/tail.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+    
+    for(var i=0; i < actual.length; i++) {
+        if(actual[i] !== expected[i])
+        {
+            
+            return false;
+        }
+    }
+    
+};
+function tail(arr)
+{
+    var resarr =[];
+    
+     for(var i = 1 ; i < arr.length; i++)
+     {
+        resarr.push(arr[i]);
+    }
+    
+    return resarr;
+    
+}
+const result = tail(["Hello","Lighthouse", "Labs"]);
+
+assertEqual(result, ["Lighthouse", "Labs"]); 
+
+//const words = ["Yo Yo", "Lighthouse", "Labs"];
+//tail(words); // no need to capture the return value since we are not checking it
+//assertEqual(words.length, 3); // original array should still have 3 elements!
+
