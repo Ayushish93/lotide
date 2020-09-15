ecefb1cd8060a656c5344fb786ed13d1b7755b58
Ayushish93 <ayushi08sharma@gmail.com>
2020-09-15 23:26:29 +0000

    completed assertArrayEqual

---
+++ lotide/assertArraysEqual.js
@@ -0,0 +1,20 @@
+function eqArrays(actual, expected) {
+    var res = true;
+    for (let i = 0; i < actual.length; i++) {
+        if(actual[i] !== expected[i]) {
+            res = false;
+        }
+    }
+    return res;
+}
+
+function assertArraysEqual(arg1,arg2) {
+    const check = eqArrays(arg1,arg2);
+    if(check === true) {
+        console.log(`Assertion Passed:  ${arg1} ===  ${arg2}`);
+    }
+    else {
+        console.log(`Assertion Failed:  ${arg1}  !==  ${arg2}`);
+    }
+}
+assertArraysEqual([1,2,3],[1,2,2]);
