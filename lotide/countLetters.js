8632f2c0998725039bbdbda0ba08a6d1552ef728
Ayushish93 <ayushi08sharma@gmail.com>
2020-09-16 21:10:48 +0000

    completed

---
+++ lotide/countLetters.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+    
+    if(actual === expected){
+        console.log(`✅Assertion Passed:  ${actual}   ===   ${expected}`);
+    }
+    else{
+        console.log(`❌ Assertion Failed: ${actual}  !==  ${expected}`);
+    }
+    
+};
+const countLetters = function(str)
+{
+    let newstr = str.split(" ").join('');
+    let returnobj = {};
+    for(let i of newstr)
+    {
+        if(returnobj.hasOwnProperty(i)) 
+        {
+            returnobj[i] += 1;
+        }
+        else
+        {
+            returnobj[i] = 1;
+        }
+    }
+    return returnobj;
+}
+console.log(countLetters("lighthouse in the house"));
