a8741d3eafb21d6fd7b0c5d996378a3f161fbe7b
Ayushish93 <ayushi08sharma@gmail.com>
2020-09-16 00:44:06 +0000

    completed

---
+++ lotide/without.js
@@ -0,0 +1,36 @@
+function eqArrays(actual, expected) {
+    var res = true;
+    for (let i = 0; i < actual.length; i++) {
+        if(actual[i] !== expected[i]) {
+            res = false;
+        }
+    }
+    return res;
+}
+
+function assertArraysEqual(arg1,arg2) {
+    const check = eqArrays(arg1,arg2);
+    if(check === true) {
+        console.log(`Assertion Passed:  ${arg1} ===  ${arg2}`);
+    }
+    else {
+        console.log(`Assertion Failed:  ${arg1}  !==  ${arg2}`);
+    }
+}
+
+function without(sourcearr,removearr)
+{
+    let resarr = [];
+    for(var i = 0; i < sourcearr.length;i++) {
+        if(!removearr.includes(sourcearr[i]))
+        {
+            resarr.push(sourcearr[i]);
+        }
+         
+    }
+    
+    return resarr;
+}
+ const words = ["hello", "world", "lighthouse"];
+ console.log(without(words, ["lighthouse"])); 
+ assertArraysEqual(words, ["hello", "world", "lighthouse"]);
