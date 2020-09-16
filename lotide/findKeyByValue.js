425a04a3be3141816cfc4059ef3c34312c582321
Ayushish93 <ayushi08sharma@gmail.com>
2020-09-16 22:02:58 +0000

    completed

---
+++ lotide/findKeyByValue.js
@@ -0,0 +1,40 @@
+const assertEqual = function(actual, expected) {
+    
+    if(actual === expected){
+        console.log(`✅ Assertion Passed:  ${actual}  " === "  ${expected}`);
+    }
+    else{
+        console.log(`❌ Assertion Failed: ${actual}  !==  ${expected}`);
+    }
+};
+const findKeyByValue = function(userOb, valOfKey)
+{
+    // for(let i in userOb)                    // using for in loop
+    // {
+    //     if(userOb[i] === valOfKey)
+    //     {
+    //         return i;
+    //     }
+    // }
+    for(let i of Object.keys(userOb))        // using for of loop with Objects.keys() method
+    {
+       
+        if(userOb[i] === valOfKey)
+        {
+            return i;
+        }
+    }
+
+}
+
+
+
+const bestTVShowsByGenre = { 
+    sci_fi: "The Expanse",
+    comedy: "Brooklyn Nine-Nine",
+    drama:  "The Wire"
+  };
+  
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
